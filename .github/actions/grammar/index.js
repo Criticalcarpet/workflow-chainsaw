83141e41d1283f4ed8e3ba10e6300a4b568ce7c5
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-01-29 11:59:16 +0530

    add workflow

--- .github/actions/grammar/index.js
+++ .github/actions/grammar/index.js
@@ -1,14 +1,12 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 try {
-    const config = yaml.load(fs.readFileSync('test.yml', 'utf8'));
+    const config = yaml.load(fs.readFileSync('grammar.yml', 'utf8'));
     const values = Object.values(config)
-    console.log(values);
     const unique = values.map((x) => {
         return x.slice(0, 1)
     })
     const flat = [].concat(...unique);
-    console.log(flat);
     for (let i = 0; i < flat.length; i++) {
        fs.writeFileSync(`dist/${flat[i]}`, '', 'utf8');
        console.log(`done : ${flat[i]}`)
