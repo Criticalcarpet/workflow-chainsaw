ac1d41bf2aac180d8cd7b4631b5a192e47839549
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-01-29 11:38:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+try {
+    const config = yaml.load(fs.readFileSync('test.yml', 'utf8'));
+    const values = Object.values(config)
+    console.log(values);
+    const unique = values.map((x) => {
+        return x.slice(0, 1)
+    })
+    const flat = [].concat(...unique);
+    console.log(flat);
+    for (let i = 0; i < flat.length; i++) {
+       fs.writeFileSync(`dist/${flat[i]}`, '', 'utf8');
+       console.log(`done : ${flat[i]}`)
+    }
+} catch (e) {
+    console.log(e);
+}
+
+
