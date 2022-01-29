9ed78daf2b51eaab4936984dd8f5f61877222aa1
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-01-29 12:13:22 +0530

    done

--- .github/actions/grammar/index.js
+++ .github/actions/grammar/index.js
@@ -1,7 +1,7 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 try {
-    const config = yaml.load(fs.readFileSync('test.yml', 'utf8'));
+    const config = yaml.load(fs.readFileSync('grammar.yml', 'utf8'));
     const values = Object.values(config)
     const unique = values.map((x) => {
         return x.slice(0, 1)
